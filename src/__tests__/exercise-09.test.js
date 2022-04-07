9a4943559b63071f6f2584c16f5947e79a977fee
JoanGauGonell <100577733+JoanGauGonell@users.noreply.github.com>
2022-04-07 13:21:26 +0200

    Delete src directory

--- src/__tests__/exercise-09.test.js
+++
@@ -1,9 +0,0 @@
-const git = require("../../utils/git");
-
-describe("exercise-09", () => {
-  test("create a new stash entry without a message", async () => {
-    let stashList = await git.stashList();
-
-    expect(stashList.total).toBe(1);
-  });
-});
--- src/__tests__/exercise-12.test.js
+++
@@ -1,9 +0,0 @@
-const git = require("../../utils/git");
-
-describe("exercise-12", () => {
-  test("clear the stash list", async () => {
-    let stashList = await git.stashList();
-
-    expect(stashList.total).toBe(0);
-  });
-});
